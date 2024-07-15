46b71a18de5a797a52235eafea2f3f922cdfd9da
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-07-15 22:56:27 +0530

    added next auth handler

--- web/src/app/api/auth/[...nextauth]/route.ts
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,7 @@
+import { NEXT_AUTH } from "@/app/lib/auth";
 import NextAuth from "next-auth";
-import CredentialsProvider  from "next-auth/providers/credentials";
-const handler = NextAuth({
-    providers:[ 
-        CredentialsProvider({
-            name:"Credentials",
-             credentials:{
-                email:{label:"email",type:"text",placeholder:'email'},
-                password:{label:"password",type:"password",placeholder:'password'}
-             },
-             async authorize(credentials:any){
-                return {
-                    id: 'user1 '
-                }
-             }
-        })
-    ]
-})
 
-export const GET = handler
-export const POST = handler
+const handler = NextAuth(NEXT_AUTH);
+
+export const GET = handler;
+export const POST = handler;
