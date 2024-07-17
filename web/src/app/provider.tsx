dba9db1ed021c3c2786abbad0a90916d93bd53dc
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-07-17 22:20:42 +0530

    add provider

--- web/src/app/provider.tsx
+++ web/src/app/provider.tsx
@@ -1,14 +1,24 @@
 "use client"
-
 import React from 'react'
 import { SessionProvider } from 'next-auth/react'
+import { RecoilRoot } from 'recoil'
+import { ThemeProvider } from '@/components/theme-provider'
+
 interface provider {
     children: React.ReactNode
 }
+
 const Provider: React.FC<provider> = ({children}) => {
   return (
     <SessionProvider>
-        {children}
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <RecoilRoot>{children}</RecoilRoot>
+      </ThemeProvider>
     </SessionProvider>
   )
 }
