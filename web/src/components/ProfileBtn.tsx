e7c4221d4942f23723ade8a4890596d656c23ea1
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-07-19 23:21:52 +0530

    add profile dropdown on btn click

--- web/src/components/ProfileBtn.tsx
+++ web/src/components/ProfileBtn.tsx
@@ -10,42 +10,41 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "./ui/button";
-import { signIn, signOut , useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import ThemeToggle from "./ThemeToggle";
 
 const ProfileBtn = () => {
-    const { data: session, status } = useSession();
-    console.log(session);
+  const { data: session, status } = useSession();
+  console.log(session);
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar>
-          <AvatarImage src={session?.user?.image || ''} />
+          <AvatarImage src={session?.user?.image || ""} />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="bg-black flex items-center flex-col text-white relative bottom-8 right-14">
+      <DropdownMenuContent className="bg-black flex items-center flex-col text-white relative bottom-8 right-14 py-2 px-3 gap-3">
         <DropdownMenuLabel>
-            {session?.user?.name ? session.user.name : "Guest"}
+          {session?.user?.name ? session.user.name : "Guest"}
         </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className="focus:bg-none bg-black hover:bg-black">
-          <Button
-            className="bg-transparent text-green-500 border-green-500"
-            variant={"outline"}
-            onClick={() => signIn()}
-          >
-            Signin
-          </Button>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Button
-            className="bg-transparent text-red-500 border-red-500"
-            variant={"outline"}
-            onClick={() => signOut()}
-          >
-            Logout
-          </Button>
-        </DropdownMenuItem>
+        <Button
+          className="bg-transparent text-green-500 border-green-500
+          h-8 w-full hover:bg-green-500/35 hover:text-green-500"
+          variant={"outline"}
+          onClick={() => signIn()}
+        >
+          Signin
+        </Button>
+        <Button
+          className="bg-transparent text-red-500 border-red-500
+          h-8 w-full hover:bg-red-500/35 hover:text-red-500"
+          variant={"outline"}
+          onClick={() => signOut()}
+        >
+          Logout
+        </Button>
+        <ThemeToggle />
       </DropdownMenuContent>
     </DropdownMenu>
   );
