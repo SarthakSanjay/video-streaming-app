d2bd11bd63505e6ac82cf07a8745ae275ee7b917
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-07-21 23:39:32 +0530

    refactored

--- web/src/components/SearchBox.tsx
+++ web/src/components/SearchBox.tsx
@@ -1,17 +1,26 @@
+"use client";
+
 import React from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { FiSearch } from "react-icons/fi";
+import { useSetRecoilState } from "recoil";
+import { searchBarAtom } from "@/store/atom";
+import { CiSearch } from "react-icons/ci";
 
 const SearchBox = () => {
+  const setToggleSearchBar = useSetRecoilState(searchBarAtom);
   return (
     <div className="w-1/3 flex h-11">
-      <Input placeholder="Search" className="hidden md:block rounded-l-full h-full" />
+      <Input
+        placeholder="Search"
+        className="hidden md:block rounded-l-full h-full focus:border-orange-500"
+      />
       <Button
+        onClick={() => setToggleSearchBar((p) => !p)}
         variant={"outline"}
         className="bg-black text-white md:rounded-r-full h-full text-3xl md:border-l-0"
       >
-        <FiSearch />
+        <CiSearch />
       </Button>
     </div>
   );
