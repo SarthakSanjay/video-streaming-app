ba1a16d262904db71bc39f1a81595e300abdfb7a
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-08-09 23:37:53 +0530

    fixed upload video functionality

--- web/src/app/studio/upload/page.tsx
+++ web/src/app/studio/upload/page.tsx
@@ -1,5 +1,7 @@
 "use client"
+// import { uploadVideoToS3 } from "@/app/actions/video"; //need to invistigate
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import React, { useRef, useState } from "react";
 
 export default function () {
@@ -11,12 +13,28 @@ export default function () {
             videoFileRef.current.click()
         }
     }
-    const handleSelectVideo = (event: any) =>{
-        const file = event.target.files[0];
-        setSelectedVideo(file);
-        const url = URL.createObjectURL(file);
-        setFileUrl(url);
-        console.log(file);
+    const uploadVideo = async(file: File) =>{
+      const res = await axios.post('/api/video' , {
+        filename: file.name,
+        contentType: file.type
+      })
+
+      if(res.status === 200){
+       const upload = await axios.put(res.data.url , file)
+        if(upload.status === 200) alert('video uploaded')
+      }
+    }
+    const handleSelectVideo = async(event: any) =>{
+       try {
+         const file = event.target.files[0];
+         setSelectedVideo(file);
+         const url = URL.createObjectURL(file);
+         setFileUrl(url);
+         console.log(file);
+         await uploadVideo(file)
+       } catch (error : any) {
+        console.log(error.message)
+       }
     }
   return (
     <div className="h-screen w-screen dark:bg-black dark:text-white flex justify-center items-center gap-5">
