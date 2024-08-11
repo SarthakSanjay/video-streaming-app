6ff6dab64e0724e78d6f2059ec262dc45c2427aa
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-08-11 22:23:26 +0530

    modified upload button added select video button

--- web/src/app/studio/upload/page.tsx
+++ web/src/app/studio/upload/page.tsx
@@ -1,46 +1,70 @@
 "use client"
 // import { uploadVideoToS3 } from "@/app/actions/video"; //need to invistigate
 import { Button } from "@/components/ui/button";
+import { fail } from "assert";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import React, { useRef, useState } from "react";
+import { Oval } from 'react-loader-spinner'
+enum uploads {
+  "no", "uploading", "uploaded"
+}
 
 export default function () {
-    const videoFileRef = useRef<HTMLInputElement | null>(null)
-    const [selectedVideo , setSelectedVideo] = useState<File | null>(null)
-    const [fileUrl , setFileUrl] = useState('')
-    const handleClick = () =>{
-        if(videoFileRef.current){
-            videoFileRef.current.click()
-        }
+  const videoFileRef = useRef<HTMLInputElement | null>(null)
+  const [selectedVideo, setSelectedVideo] = useState<File | null>(null)
+  const [isUploaded, setIsUploaded] = useState<uploads>(uploads.no)
+  const [fileUrl, setFileUrl] = useState('')
+  const router = useRouter()
+  const handleClick = () => {
+    if (videoFileRef.current) {
+      videoFileRef.current.click()
     }
-    const uploadVideo = async(file: File) =>{
-      const res = await axios.post('/api/video' , {
-        filename: file.name,
-        contentType: file.type
-      })
-
-      if(res.status === 200){
-       const upload = await axios.put(res.data.url , file)
-        if(upload.status === 200) alert('video uploaded')
+  }
+  const uploadVideo = async () => {
+    setIsUploaded(uploads.uploading)
+    const res = await axios.post('/api/video', {
+      filename: selectedVideo?.name,
+      contentType: selectedVideo?.type
+    })
+    setIsUploaded(uploads.uploading)
+    if (res.status === 200) {
+      const upload = await axios.put(res.data.url, selectedVideo)
+      if (upload.status === 200) {
+        setIsUploaded(uploads.uploaded)
       }
     }
-    const handleSelectVideo = async(event: any) =>{
-       try {
-         const file = event.target.files[0];
-         setSelectedVideo(file);
-         const url = URL.createObjectURL(file);
-         setFileUrl(url);
-         console.log(file);
-         await uploadVideo(file)
-       } catch (error : any) {
-        console.log(error.message)
-       }
+  }
+  const handleSelectVideo = async (event: any) => {
+    try {
+      const file = event.target.files[0];
+      setSelectedVideo(file);
+      const url = URL.createObjectURL(file);
+      setFileUrl(url);
+      console.log(file);
+    } catch (error: any) {
+      console.log(error.message)
     }
+  }
+
   return (
     <div className="h-screen w-screen dark:bg-black dark:text-white flex justify-center items-center gap-5">
-        {fileUrl && <video className="h-full w-1/2" controls src={fileUrl}></video>}
-        <input ref={videoFileRef} type="file" accept="video/*" className="hidden" onChange={handleSelectVideo} />
-      <Button onClick={handleClick}>Upload</Button>
+      {fileUrl && <video className="h-full w-1/2" controls src={fileUrl}></video>}
+      <input ref={videoFileRef} type="file" accept="video/*" className="hidden" onChange={handleSelectVideo} />
+      <Button onClick={handleClick}
+        className={`${selectedVideo ? 'bg-blue-500' : ''}`}
+      >{selectedVideo ? "Selected" : "Select Video"}</Button>
+      <Button
+        className={`${isUploaded === uploads.uploaded ? "bg-green-500" : ""} `}
+        onClick={uploadVideo}
+        disabled={isUploaded === uploads.uploaded ? true : false}
+      >
+        {isUploaded === uploads.uploading ? <Oval height="20" strokeWidth="8" /> : ""}
+        {isUploaded === uploads.uploaded ? "Uploaded" : "Upload"}
+      </Button>
+      {
+        isUploaded === uploads.uploaded ? <Button onClick={() => router.push("/studio")}>Go to Studio</Button> : ''
+      }
     </div>
   );
 }
