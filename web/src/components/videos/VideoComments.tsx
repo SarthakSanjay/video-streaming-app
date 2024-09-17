f7ea7769ca14faef714109397c79a9180102c5c5
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-09-17 23:45:08 +0530

    refactored

--- web/src/components/videos/VideoComments.tsx
+++ web/src/components/videos/VideoComments.tsx
@@ -3,6 +3,16 @@ import { Input } from '../ui/input'
 import { comment } from 'postcss'
 import { ChevronDown } from 'lucide-react'
 
+interface Reply {
+  name: string;
+  comment: string;
+}
+
+interface Comment {
+  name: string;
+  comment: string;
+  replies?: Reply[];  // Optional, as not all comments have replies
+}
 const VideoComments = () => {
   const [toggleReplies, setToggleReplies] = useState(false)
   const comments = [
@@ -10,9 +20,11 @@ const VideoComments = () => {
     {
       name: "marko", comment: "good but expected more", replies: [
         { name: "jacob", comment: "yes agreed" },
-        { name: "mad", comment: "ya it's decent" }
+        { name: "mad", comment: "ya it's decent" },
+        { name: "shinchan", comment: "ab main etna bhi khas nhi" }
       ]
-    }
+    },
+    { name: "doraemaon", comment: "I love doracake" }
   ]
   return (
     <div className='h-max w-full'>
@@ -30,17 +42,7 @@ const VideoComments = () => {
             <p className='ml-14'>{comment.comment}</p>
             <button className='flex text-violet-500 ml-14'
               onClick={() => setToggleReplies(!toggleReplies)}>{comment.replies && <ChevronDown />} {comment.replies?.length}</button>
-            {toggleReplies ? <div className='ml-14'>{
-              comment.replies?.map((comment) => {
-                return <div className='h-max w-full'>
-                  <div className='h-14 w-full flex gap-3 items-center'>
-                    <div className='h-12 w-12 bg-blue-600 rounded-full'></div>
-                    <h1>{comment.name}</h1>
-                  </div>
-                  <p className='ml-14'>{comment.comment}</p>
-                </div>
-              })
-            }</div> : ""}
+            {toggleReplies ? <Replies comment={comment} /> : ""}
           </div>
         })}
       </div>
@@ -48,4 +50,20 @@ const VideoComments = () => {
   )
 }
 
+const Replies = ({ comment }: { comment: Comment }) => {
+  return <div className='ml-14'>{
+    comment.replies?.map((comment) => {
+      return <div className='h-max w-full'>
+        <div className='h-14 w-full flex gap-3 items-center'>
+          <div className='h-12 w-12 bg-blue-600 rounded-full'></div>
+          <h1>{comment.name}</h1>
+        </div>
+        <p className='ml-14'>{comment.comment}</p>
+      </div>
+    })
+  }</div>
+}
+
+
 export default VideoComments
+
