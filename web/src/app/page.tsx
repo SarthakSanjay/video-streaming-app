e39d2c9e536c386158c2591368172266634b7389
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-09-30 23:14:37 +0530

    removed unnecessary loc

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,29 +2,27 @@ import Appbar from "../components/Appbar";
 import { getServerSession } from "next-auth";
 import { NEXT_AUTH } from "./lib/auth";
 import Content from "@/components/Content";
-import Signin from "@/components/Signin";
 import LandingPage from "@/components/LandingPage";
 
-async function getUserDetails(){
+async function getUserDetails() {
   const session = await getServerSession(NEXT_AUTH);
-
   return session
 }
 
 export default async function Home() {
   const session = await getUserDetails()
 
-  if(session?.user){
+  if (session?.user) {
     console.log(session?.user);
-    return <div className="h-screen w-screen flex flex-col dark:bg-black">
-    <Appbar />
-    <Content />
-  </div>
+    return <div className="h-screen w-screen flex flex-col dark:bg-gradient-to-r from-black via-[#130837] to-black items-center">
+      <Appbar />
+      <Content />
+    </div>
   }
 
   return (
     <div className="h-max w-screen flex flex-col dark:bg-black">
-     <LandingPage />
+      <LandingPage />
     </div>
   );
 }
