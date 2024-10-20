680a9e3e4f193f2c988ae6a32eb4bb9272f6197c
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-10-20 23:42:31 +0530

    added get video detail by id server action

--- web/src/app/actions/video.ts
+++ web/src/app/actions/video.ts
@@ -2,17 +2,7 @@
 import { putObject } from "@/aws/aws";
 import prisma from "@/db";
 
-enum FeedbackType {
-  LIKE = "LIKE",
-  DISLIKE = "DISLIKE"
-}
-
-enum ContentType {
-  VIDEO = "VIDEO",
-  COMMENT = "COMMENT",
-  REPLY = "REPLY"
-}
-
+// get all videos
 export async function getVideos() {
   const videos = await prisma.videoMetadata.findMany()
   return videos
@@ -51,150 +41,17 @@ export async function setVideoMetadata(title: string, description: string, thumb
   return 'video metadata added'
 }
 
-export async function feedback(contentId: number, contentType: ContentType, userId: number, feedbackType: FeedbackType) {
-  console.log('data', contentId, contentType, userId, feedbackType)
-  console.log(contentType === ContentType.VIDEO, contentType, ContentType.VIDEO)
-  //video
-  if (contentType === ContentType.VIDEO) {
-    const video = await prisma.videoMetadata.findFirst({
-      where: {
-        id: contentId
-      }
-    })
+export async function getVideoDetailById(videoId: number) {
+  const video = await prisma.videoMetadata.findUnique({
+    where: { id: videoId }
+  })
 
-    if (!video) return "video not available"
-
-    const feedback = await prisma.feedback.findFirst({
-      where: { userId: userId, videoId: contentId }
-    })
-
-    if (feedback) {
-      await prisma.videoMetadata.update({
-        where: { id: contentId },
-        data: {
-          likes: feedbackType === FeedbackType.LIKE ? { increment: 1 } : { decrement: 1 },
-          dislikes: feedbackType === FeedbackType.DISLIKE ? { increment: 1 } : { decrement: 1 }
-        }
-      })
-    } else {
-      await prisma.feedback.create({
-        data: {
-          userId: userId,
-          videoId: contentId,
-          feedbackType: feedbackType
-        }
-      })
-    }
-
-
-
-    return "video liked"
+  if (!video) {
+    return "no video found with this videoId"
   }
 
-  //comment
-  if (contentType === ContentType.COMMENT) {
-    const comment = await prisma.comments.findFirst({
-      where: { id: contentId }
-    })
-    if (!comment) return "no comment found"
-
-    // contentId contentType userId feedbackType
-
-    const feedback = await prisma.feedback.findUnique({
-      where: {
-        commentId_userId: {
-          commentId: contentId,
-          userId: userId
-        }
-      }
-    })
-
-    if (!feedback) {
-      //create feedback
-      // feedback type likes
-
-      await prisma.feedback.create({
-        data: {
-          commentId: contentId,
-          userId: userId,
-          feedbackType: feedbackType
-        }
-      })
-
-      if (feedbackType === FeedbackType.LIKE) {
-        await prisma.comments.update({
-          where: { id: contentId },
-          data: {
-            likes: { increment: 1 }
-          }
-        })
-      } else {
-        await prisma.comments.update({
-          where: { id: contentId },
-          data: {
-            dislikes: { increment: 1 }
-          }
-        })
-      }
-      return "comment liked"
-    }
-
-    await prisma.feedback.delete({
-      where: {
-        commentId_userId: {
-          commentId: contentId,
-          userId: userId
-        }
-      }
-    })
-
-    if (feedbackType === FeedbackType.DISLIKE) {
-      await prisma.comments.update({
-        where: { id: contentId },
-        data: {
-          likes: { decrement: 1 }
-        }
-      })
-    } else {
-      await prisma.comments.update({
-        where: { id: contentId },
-        data: {
-          dislikes: { decrement: 1 }
-        }
-      })
-    }
-  }
-
-  return "comment unliked"
+  return video
 }
-
-//reply
-// if (contentType === ContentType.REPLY) {
-//   const reply = await prisma.replies.findFirst({
-//     where: { id: contentId }
-//   })
-//
-//   if (!reply) return "reply not available"
-//
-//   await prisma.feedback.create({
-//     data: {
-//       userId: userId,
-//       replyId: contentId,
-//       feedbackType: feedbackType
-//     }
-//   })
-//
-//   await prisma.replies.update({
-//     where: { id: contentId },
-//     data: {
-//       likes: feedbackType === FeedbackType.LIKE ? { increment: 1 } : { decrement: 1 },
-//       dislikes: feedbackType === FeedbackType.DISLIKE ? { increment: 1 } : { decrement: 1 }
-//     }
-//   })
-//
-//   return "reply liked"
-// }
-
 
 
 
